2e8ca96d37a69a00a641c1681d4ef246dd02a2e9
Varungunda60 <varungunda60@gmail.com>
2025-03-30 04:31:08 -0700

    Initial commit

---
+++ src/test/java/com/varun/ClaimProcessingApplication/ClaimProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.ClaimProcessingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
